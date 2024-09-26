0ac82c4db31ee42b753a5593072cef92e11e4ad5
Umangpatel5122006 <u24ai041@aid.svnit.ac.in>
2024-09-26 11:59:26 +0530

    Delete 12-asciivalue.c

--- 12-asciivalue.c
+++
@@ -1,9 +0,0 @@
-// this programe is designed to display ASCII value of given number
-#include <stdio.h>
-void main()
-{
-	char ch;
-	printf("Enter a character: ");
-	scanf("%c",&ch);
-	printf("The ASCII value of %c is: %d\n" , ch,ch);//Printing value of entered character
-}
