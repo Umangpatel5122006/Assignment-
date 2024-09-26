ad60822dad79340f50f1f533366f4e28061c3bc7
Umangpatel5122006 <u24ai041@aid.svnit.ac.in>
2024-09-26 12:00:18 +0530

    Delete 19-checksmallletter.c

--- 19-checksmallletter.c
+++
@@ -1,9 +0,0 @@
-// this programe is designed to check weather entered letter is lower case or not
-#include<stdio.h>
-void main()
-{
-	char ch;
-	printf("Enter a character \n");
-	scanf("%c",&ch);
-	(ch>='a' && ch<='z')?printf("%c is a small case letter  \n",ch):printf("%c is not a small case letter  \n",ch);
-}
